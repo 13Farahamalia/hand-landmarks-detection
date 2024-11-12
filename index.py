36cd21add22df9ef72147412504d0185168d52d2
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-11-12 09:26:18 +0700

    feat: hand landmarks detection

---
+++ index.py
@@ -0,0 +1,19 @@
+from HandTracing import HandDetector
+
+import cv2
+
+handDetector = HandDetector(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
+webcam = cv2.VideoCapture()
+webcam.open(0, cv2.CAP_DSHOW)
+
+while True:
+    status, frame = webcam.read()
+    frame = cv2.flip(frame, 1)
+    handLandMark = handDetector.findHandLandmarks(image=frame, draw=True)
+
+    cv2.imshow("hand Landmark", frame)
+    if cv2.waitKey(1) == ord('a'):
+        break
+cv2.destroyAllWindows()
+webcam.release()
